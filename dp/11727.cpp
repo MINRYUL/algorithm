b51c8fca8242f596d5011911a21098f3403c206d
MINRYUL <55181961+MINRYUL@users.noreply.github.com>
2021-01-30 22:48:28 +0900

    dp

---
+++ dp/11727.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    int x, dp[1001];    cin >> x;
+    dp[1] = 1;  dp[2] = 3;  dp[3] = 5;
+    for (int i = 3; i <= x; i++)    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2]) % 10007;
+
+    cout << dp[x] << endl;
+    return 0;
+}
